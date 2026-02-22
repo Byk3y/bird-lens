025efc5252ddc3fadb35223d716ebdcd79c11e4f
Francis <scenes_pineal.0d@icloud.com>
2026-02-22 13:02:38 +0100

    feat: add Poppins-Bold font and load in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    PoppinsBold: require('../assets/fonts/Poppins-Bold.ttf'),
     ...FontAwesome.font,
   });
 
